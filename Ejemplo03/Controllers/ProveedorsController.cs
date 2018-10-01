9b39d35517072942279e88a83d51e99a16598183
Rodrigo <exit>
2018-10-01 11:03:28 -0500

    primeros cambios

---
+++ Ejemplo03/Controllers/ProveedorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ejemplo03;
+
+namespace Ejemplo03.Controllers
+{
+    public class ProveedorsController : Controller
+    {
+        private EjemploMVCEntities db = new EjemploMVCEntities();
+
+        // GET: Proveedors
+        public ActionResult Index()
+        {
+            return View(db.Proveedor.ToList());
+        }
+
+        // GET: Proveedors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proveedors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdProveedor,Apellido,Nombre,Ruc,Credito")] Proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Proveedor.Add(proveedor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // POST: Proveedors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdProveedor,Apellido,Nombre,Ruc,Credito")] Proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proveedor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // POST: Proveedors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Proveedor proveedor = db.Proveedor.Find(id);
+            db.Proveedor.Remove(proveedor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Ejemplo03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejemplo03.MvcApplication" Language="C#" %>
